d6b938045e1b7fece61d342ed64e0791cef10dc3
arpitSatpute <arpitrameshsatpute6986@gmail.com>
2025-03-31 10:50:49 +0530

    Scheduler service Fix

--- src/main/java/com/arpit/taskk/controller/WeeklyScoreController.java
+++ src/main/java/com/arpit/taskk/controller/WeeklyScoreController.java
@@ -1,13 +1,11 @@
 package com.arpit.taskk.controller;
 
 import com.arpit.taskk.dto.WeeklyScoreDTO;
-import com.arpit.taskk.entity.enums.Role;
-import com.arpit.taskk.services.ScheduledService;
+
 import com.arpit.taskk.services.WeeklyScoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
--- src/main/java/com/arpit/taskk/services/ScheduledService.java
+++ src/main/java/com/arpit/taskk/services/ScheduledService.java
@@ -1,13 +1,16 @@
 package com.arpit.taskk.services;
 
+import com.arpit.taskk.entity.DailyScore;
 import com.arpit.taskk.entity.Task;
 import com.arpit.taskk.entity.WeeklyScore;
 import com.arpit.taskk.entity.enums.Status;
+import com.arpit.taskk.repository.DailyScoreRepository;
 import com.arpit.taskk.repository.TaskRepository;
 import com.arpit.taskk.repository.WeeklyScoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -18,23 +21,30 @@ public class ScheduledService {
 
     private final WeeklyScoreRepository weeklyScoreRepository;
     private final TaskRepository taskRepository;
+    private final DailyScoreService dailyScoreService;
+    private final DailyScoreRepository dailyScoreRepository;
 
     @Scheduled(cron = "0 0 0 * * ?")
     public void updateWeeklyScore() {
         List<WeeklyScore> weeklyScores = weeklyScoreRepository.findAll();
         for (WeeklyScore weeklyScore : weeklyScores) {
-            weeklyScore.setDay7(weeklyScore.getDay6());
-            weeklyScore.setDay6(weeklyScore.getDay5());
-            weeklyScore.setDay5(weeklyScore.getDay4());
-            weeklyScore.setDay4(weeklyScore.getDay3());
-            weeklyScore.setDay3(weeklyScore.getDay2());
-            weeklyScore.setDay2(weeklyScore.getDay1());
-            weeklyScore.setDay1(BigDecimal.ZERO);
-            weeklyScoreRepository.save(weeklyScore);
+            DailyScore dailyScore = dailyScoreRepository.findByUserId(weeklyScore.getUser().getId());
+            if (dailyScore != null) {
+                weeklyScore.setDay7(weeklyScore.getDay6());
+                weeklyScore.setDay6(weeklyScore.getDay5());
+                weeklyScore.setDay5(weeklyScore.getDay4());
+                weeklyScore.setDay4(weeklyScore.getDay3());
+                weeklyScore.setDay3(weeklyScore.getDay2());
+                weeklyScore.setDay2(weeklyScore.getDay1());
+                weeklyScore.setDay1(dailyScore.getCurrScore());
+            }
         }
+        weeklyScoreRepository.saveAll(weeklyScores);
     }
 
-    @Scheduled(cron = "0 2 0 * * ?")
+
+    @Scheduled(cron = "0 5 0 * * ?")
+    @Transactional
     public void updateTaskList() {
         taskRepository.deleteAllByStatus(Status.COMPLETED);
         List<Task> tasks = taskRepository.findAllByStatus(Status.ONGOING);
